186510f2ed525bac53011ee88b016624c784ef79
roberto <rofe@influencerindex.com>
2022-06-01 17:07:01 +0100

    final

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class _MainPage extends StatelessWidget {
+class _MainPage extends StatefulWidget {
   const _MainPage({Key? key}) : super(key: key);
 
   @override
+  State<_MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<_MainPage> {
+  int _numberClicks = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: () {
+            setState((){                       
+              _numberClicks++;
+            });
+          },
+          child: Text(
+            '$_numberClicks',
+            style: const TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
   }
 }
